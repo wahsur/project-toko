56f79beb518546b2bb0bdf814b56e55eca3fe4e4
wahsur <ngurahsurya736@gmail.com>
2025-07-16 23:24:28 +0800

    update icon

--- app/src/main/java/com/example/toko/api/apiClient.java
+++ app/src/main/java/com/example/toko/api/apiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class apiClient {
-    private static final String BASE_URL = "http://10.0.2.2:8000/api/";
+    private static final String BASE_URL = "https://toko.mypasti.my.id/api/";
     private static Retrofit retrofit;
 
     public static Retrofit getClient() {
